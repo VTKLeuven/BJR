2d0a9d28a30a5c805221205b1d0dca7657e61577
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-10-25 23:02:51 +0200

    Initial commit: nextjs installation + sidebar

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html>
+        <body>
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                {children}
+            </main>
+        </SidebarProvider>
+        </body>
+        </html>
+    )
+}
